6bda48a2eb4c5842dd7e211b067b1888fe99104b
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2020-10-01 06:12:09 +0530

    Create clipboardhijacking.py

---
+++ clipboardhijacking.py
@@ -0,0 +1,18 @@
+import pyperclip
+import time
+
+list = []
+
+while True:
+    
+    if pyperclip.paste() != 'None':
+        value = pyperclip.paste()
+        #print pyperclip.paste()
+
+
+        if value not in list:  
+            
+            list.append(value)
+        print list
+        
+        time.sleep(3)
